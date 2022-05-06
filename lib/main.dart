66a4057637ffd56feb7bb9559772ea78d72cb814
prasetion <prasetion@gmail.com>
2022-05-07 04:09:01 +0700

    add drawer

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/screens/category_meals_screen.dart';
+import 'package:meals_app/screens/filters_screen.dart';
 import 'package:meals_app/screens/meal_detail_screen.dart';
 import 'package:meals_app/screens/tabs_screen.dart';
 import 'screens/categories_screen.dart';
@@ -34,6 +35,7 @@ class MyApp extends StatelessWidget {
         "/": (ctx) => TabScreen(),
         CategoryMealsScreen.routeName: (ctx) => CategoryMealsScreen(),
         MealDetailScreen.routeName: (ctx) => MealDetailScreen(),
+        FiltersScreen.routeName: (ctx) => FiltersScreen(),
       },
       onGenerateRoute: (settings) {
         // return MaterialPageRoute(builder: builder)
